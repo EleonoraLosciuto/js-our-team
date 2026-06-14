7683b8b1bf224a591a0ea060c27a07f01f505bed
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-06-14 14:19:53 +0200

    fix main.js with renderTeam function

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,28 @@
 const memberCards = document.getElementById('cardsSection');
-console.table(teamMembers);
 
 
-let htmlCards = "";
-
-for (let i = 0; i < teamMembers.length; i++) {
-     htmlCards += `
+const teamMemberCardTemp = person => 
+`
         <div class="team-member">
-            <img src="./assets/${teamMembers[i].img}" alt="team-member pic">
+            <img src="./assets/${person.img}" alt="team-member pic">
             <div class="info-member">
-                <p>${teamMembers[i].name}</p>
-                <p>${teamMembers[i].role}</p>
-                <a class="email" href="mailto:${teamMembers[i].email}" >${teamMembers[i].email}</a>
+                <p>${person.name}</p>
+                <p>${person.role}</p>
+                <a class="email" href="mailto:${person.email}" >${person.email}</a>
             </div>
         </div>
-        `
-    };
+        `;
 
-memberCards.innerHTML = htmlCards;
+
+const renderTeam = () => {
+    let htmlCards = "";
+    
+    for (let i = 0; i < teamMembers.length; i++) { // note: alternative option: for (const member of teamMembers)
+    htmlCards += teamMemberCardTemp(teamMembers[i]);
+}
+    memberCards.innerHTML = htmlCards;
+};
+
+renderTeam();
 
 
